1f80f70ea5c1da8496f44087a0b99ed1ce40a213
Kunal Chand <kunalchand234@gmail.com>
2023-10-22 15:15:49 -0400

    Weekly 367 & Barclays-OA

---
+++ Weekly-367/Q1.java
@@ -0,0 +1,15 @@
+public class Q1 {
+    public int[] findIndices(int[] nums, int indexDifference, int valueDifference) {
+        int[] ans = {-1,-1};
+        for(int i=0; i<nums.length; i++){
+            for(int j=i; j<nums.length; j++){
+                if(Math.abs(i-j) >= indexDifference && Math.abs(nums[i]-nums[j]) >= valueDifference){
+                    ans[0] = i;
+                    ans[1] = j;
+                    return ans;
+                }
+            }
+        }
+        return ans;
+    }
+}
